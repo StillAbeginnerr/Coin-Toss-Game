df526738934935f47a18a9ce7d6c9b6975f28276
shivam agarwal <shivamagarwaloff@gmail.com>
2022-04-21 21:44:20 +0530

    complete

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-const buttons = document.querySelectorAll('button');
-let heads=1;
-let tails=0;
-let playerscore=0;
-let computerscore=0;
+//Variables
+let player_score=0;
+let Computer_score=0;
+const btn = document.getElementById('head');
+const score = document.getElementById('score');
+const tail = document.getElementById('tail');
+const Heads = document.getElementsByClassName('choosenHead');
+let P_score = document.getElementById('playerScore');
+let C_score = document.getElementById('computerscore');
+// logic
 
-function displaySel(player,computer){
-    const playerSel = document.querySelector('#player');
-    const ComputerSel = document.querySelector('#computer');
-if(user==='heads'){
-    playerSel.style.color='red';
-}
-if(user==='tails'){
-    playerSel.style.color='blue';
-}
-if(computer==='heads'){
-    ComputerSel.style.color='green';
-}
-if(computer==='heads'){
-    ComputerSel.style.color='yellow';
-}
+
+function Int(max) {
+    return Math.floor(Math.random() * max);
+  }
+function headToTail()
+{
+ if((Int(2))%2==0)
+ {
+     console.log("heads");
+score.innerHTML="Heads";
+     btn.style.display= "initial";
+     tail.style.display= "none";
+     player_score+=1;
+     if(player_score<=4){
+     P_score.innerHTML=player_score;
+     }else{
+         P_score.innerHTML="You WON THE GAME";
+         C_score.innerHTML="Computer Lost";
+         player_score=0;
+            Computer_score=0;
+     }
+ }else{
+     console.log("tails");
+     score.innerHTML="Tails";
+     tail.style.display= "initial";
+     btn.style.display= "none";
+     Computer_score+=1;
+    if(Computer_score<=4){
+        C_score.innerHTML=Computer_score;
+        }else{
+            C_score.innerHTML="Computer WON THE GAME";
+            P_score.innerHTML="You lost";
+            player_score=0;
+            Computer_score=0;
+        }
+ }   
 }
